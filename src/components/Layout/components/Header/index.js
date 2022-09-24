6ef8f0c3af5422d4af4c08888b0a84e067ac0493
AnhCT8 <caoanh453@gmail.com>
2022-09-24 23:32:41 +0700

    Header

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -3,23 +3,26 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleXmark, faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 import styles from './Header.module.scss';
 import images from '~/assets/images';
+import Tippy from '@tippyjs/react/headless';
+// import 'tippy.js/dist/tippy.css';
 const cx = classNames.bind(styles);
 function Header() {
     return (
         <header className={cx('wrapper')}>
             <div className={cx('inner')}>
-                <div className={cx('logo')}>
-                    <img src={images.logo} alt="tiktok" />
-                </div>
+                <img src={images.logo} alt="tiktok" />
                 <div className={cx('search')}>
                     <input placeholder="Search accounts or videos" spellCheck={false} />
                     <button className={cx('clear')}>
                         <FontAwesomeIcon icon={faCircleXmark} />
                     </button>
                     <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
-                    <button className={cx('search-btn')}>
-                        <FontAwesomeIcon icon={faMagnifyingGlass} />
-                    </button>
+
+                    <Tippy content="Tìm kiếm" placement="right">
+                        <button className={cx('search-btn')}>
+                            <FontAwesomeIcon icon={faMagnifyingGlass} />
+                        </button>
+                    </Tippy>
                 </div>
                 <div className={cx('actions')}></div>
             </div>
